593b33e37e41efe964dd291da0449a913b050b51
showay <df405102@gmail.com>
2017-10-11 18:50:16 +0800

    Works nice, good visual effect

--- CartPole-v0/CartPole-v0.py
+++ CartPole-v0/CartPole-v0.py
@@ -9,10 +9,10 @@ from keras.optimizers import Adam
 from keras.initializers import TruncatedNormal, glorot_normal, Ones, lecun_uniform
 
 class DQNCartPoleSolver():
-    def __init__(self, n_episodes=1000, n_win_ticks=195, max_env_steps=None, gamma=1.0, epsilon=1.0, epsilon_min=0.01,
-                 epsilon_log_decay=0.937, alpha=0.01, alpha_decay=0.01, batch_size=64, monitor=False, quiet=False):
+    def __init__(self, n_episodes=10000, n_win_ticks=5000, max_env_steps=None, gamma=1.0, epsilon=1.0, epsilon_min=0.0,
+                 epsilon_log_decay=0.9, alpha=0.03, alpha_decay=0.005, batch_size=64, monitor=False, quiet=False):
         self.memory = deque(maxlen=100000)
-        self.env = gym.make('CartPole-v0')
+        self.env = gym.make('CartPole-v1')
         if monitor: self.env = gym.wrappers.Monitor(self.env, '../data/cartpole-1', force=True)
         self.gamma = gamma
         self.epsilon = epsilon
@@ -29,7 +29,7 @@ class DQNCartPoleSolver():
         # Init model
         self.model = Sequential()
         self.model.add(Dense(72, input_dim=4, activation='tanh', kernel_initializer='glorot_normal', bias_initializer='glorot_normal'))
-        self.model.add(Dense(144, activation='tanh', kernel_initializer='glorot_normal', bias_initializer='glorot_normal'))
+        self.model.add(Dense(144, activation='tanh', kernel_initializer='glorot_normal'))
         self.model.add(Dense(2, activation='linear'))
         self.model.compile(loss='mse', optimizer=Adam(lr=self.alpha, decay=self.alpha_decay))
 
@@ -77,35 +77,47 @@ class DQNCartPoleSolver():
 
         for e in range(self.n_episodes):
             state = self.preprocess_state(self.env.reset())
+            #print('init:', state)
             done = False
             i = 0
-            self.env.render()
+
             while not done:
+
+                if i>=50 or i == 0: self.env.render()
                 action = self.choose_action(state, self.get_epsilon(e))
                 next_state, reward, done, _ = self.env.step(action)
                 next_state = self.preprocess_state(next_state)
+                #reward -= abs(next_state[0][0]) # x
+                if abs(next_state[0][1]) <= abs(state[0][1]): reward += 0.1
+                else: reward -= 0.1
+                #print(state)
                 self.remember(state, action, reward, next_state, done)
                 state = next_state
                 i += 1
 
+            print('Ep No. %d, score %d' %(e+1, i))
 
             scores.append(i)
             mean_score = np.mean(scores)
             if mean_score >= self.n_win_ticks and e+1 >= 100:
                 if not self.quiet: print('Ran {} episodes. Solved after {} trials ✔'.format(e+1, e+1 - 100))
+                input()
                 return e - 100
             if (e+1) % 100 == 0 and not self.quiet:
                 print('[Episode {}] - Mean survival time over last 100 episodes was {} ticks.'.format(e+1, mean_score))
 
-            self.replay(self.batch_size)
 
-            if e + 1 > 300: train_target = 50
+            self.replay(self.batch_size)
+            if   e + 1 > 2000: train_target = 1000
+            elif e + 1 > 1000: train_target = 250
+            elif e + 1 > 800: train_target = 200
+            elif e + 1 > 500: train_target = 100
+            elif e + 1 > 300: train_target = 50
             elif e + 1 > 200: train_target = 35
             elif e + 1 > 100: train_target = 20
             elif e + 1 > 50: train_target = 10
 
             if (e+1) % train_target == 0:
-
                 self.replace_target()
 
         if not self.quiet: print('Did not solve after {} episodes 😞'.format(e))
